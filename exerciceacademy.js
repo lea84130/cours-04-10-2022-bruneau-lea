b13a3671c666c85afc1819256785cfd7145a2a5e
Lea Bruneau <blea84130@gmail.com>
2022-10-04 11:21:21 +0200

    j'ai envoyé le travail sur les variables

---
+++ exerciceacademy.js
@@ -0,0 +1,49 @@
+var favoriteFood = "pizza";
+var numOfSlices = 8;
+console.log(favoriteFood);
+console.log (numOfSlices);
+
+let changeMe = false;
+console.log(changeMe);
+
+const entree = 'Enchiladas';
+console.log(entree);
+entree = 'Tacos'
+
+let levelUp = 10;
+let powerLevel = 9001;
+let multiplyMe = 32;
+let quarterMe = 1152;
+
+// Use the mathematical assignments in the space below:
+
+levelUp +=5
+console.log(levelUp);
+
+powerLevel -=100;
+console.log(100);
+
+multiplyMe *=11;
+console.log(11);
+
+quarterMe /=4;
+console.log(4);
+
+let gainedDollar = 3;
+let lostDollar = 50;
+
+gainedDollar++;
+lostDollar--;
+
+let favoriteAnimal = 'chien';
+console.log('My favorite animal:' + favoriteAnimal);
+
+let myName = 'lea';
+let myCity = 'newyork';
+
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`)
+
+
+console.log(typeof newVariable);
+newVariable = 1;
+console.log(typeof newVariable);
