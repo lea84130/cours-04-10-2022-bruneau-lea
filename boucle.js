c56db9a108ca12edf9c06470ac5cdaefa571da3b
Lea Bruneau <blea84130@gmail.com>
2022-10-10 10:23:02 +0200

    J'ai envoyé l'exercice 5

--- boucle.js
+++ boucle.js
@@ -25,3 +25,15 @@ for (let i = 0; i < bobsFollowers.length; i++) {
     }
   }
 };
+
+
+const cards = ['diamond', 'spade', 'heart', 'club'];
+
+// Write your code below
+let currentCard;
+
+while ( currentCard != 'spade') {
+  currentCard 
+  = cards[Math.floor(Math.random() * 4)];
+	console.log(currentCard);
+}
