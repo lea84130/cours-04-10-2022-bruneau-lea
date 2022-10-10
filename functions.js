6c2d0217cbdc2bbe84d28a5ae67e00080f788b1e
Lea Bruneau <blea84130@gmail.com>
2022-10-10 09:01:33 +0200

    J'ai envoyé l'exercice 3

--- functions.js
+++ functions.js
@@ -15,3 +15,9 @@ function sayThanks() {
 sayThanks();
 sayThanks();
 sayThanks();
+
+
+function sayThanks(name) {
+  console.log('Thank you for your purchase '+ name + '! We appreciate your business.');
+}
+ sayThanks('Cole');
