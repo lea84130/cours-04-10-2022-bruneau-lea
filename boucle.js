33eda1d6dbf80133ef2ee50f56c50127b26956aa
Lea Bruneau <blea84130@gmail.com>
2022-10-10 10:20:38 +0200

    J'ai envoyé l'exercice 4

--- boucle.js
+++ boucle.js
@@ -12,3 +12,16 @@ for (let counter = 5; counter < 11; counter++) {
 for (let counter = 3; counter >= 0; counter--){
   console.log(counter);
 }
+
+
+let bobsFollowers = ['Lea', 'Julia', 'Jules', 'Enzo'];
+let tinasFollowers = ['Jules', 'Lea', 'Yoann'];
+let mutualFollowers = [];
+
+for (let i = 0; i < bobsFollowers.length; i++) {
+  for (let j = 0; j < tinasFollowers.length; j++) {
+    if (bobsFollowers[i] === tinasFollowers[j]) {
+      mutualFollowers.push(bobsFollowers[i]);
+    }
+  }
+};
