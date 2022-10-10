fd0d03756ca9c78918f4f739e8cde8f0aafdb262
Lea Bruneau <blea84130@gmail.com>
2022-10-10 11:03:11 +0200

    J'ai envoyé l'exercice 6

--- tableaux.js
+++ tableaux.js
@@ -31,3 +31,8 @@ console.log(condiments);
 utensils[3] = 'Spoon';
 
 console.log(utensils);
+
+
+const objectives = ['Learn a new language', 'Read 52 books', 'Run a marathon'];
+
+console.log(objectives.length);
