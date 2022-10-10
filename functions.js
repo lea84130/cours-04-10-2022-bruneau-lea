5beb2c43e0f36dbd95bdce68ae6197fc797bf1e2
Lea Bruneau <blea84130@gmail.com>
2022-10-10 09:12:00 +0200

    J'ai envoyé l'exercice 4

--- functions.js
+++ functions.js
@@ -21,3 +21,12 @@ function sayThanks(name) {
   console.log('Thank you for your purchase '+ name + '! We appreciate your business.');
 }
  sayThanks('Cole');
+
+
+ function monitorCount(rows, columns) {
+  return rows * columns;
+}
+
+const numOfMonitors = monitorCount(5, 4);
+
+console.log(numOfMonitors);
