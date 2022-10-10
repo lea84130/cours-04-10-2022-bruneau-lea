a235b6499b728ad914f97ee23168b1fa41304a1f
Lea Bruneau <blea84130@gmail.com>
2022-10-10 09:15:11 +0200

    J'ai envoyé l'exercice 5

--- functions.js
+++ functions.js
@@ -30,3 +30,15 @@ function sayThanks(name) {
 const numOfMonitors = monitorCount(5, 4);
 
 console.log(numOfMonitors);
+
+function monitorCount(rows, columns) {
+  return rows * columns;
+}
+
+function costOfMonitors(rows, columns) {
+ return monitorCount(rows, columns)  * 200;
+}
+
+const totalCost = costOfMonitors(5, 4);
+
+console.log(totalCost);
